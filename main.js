b02d2287ae997d59314f68e666398f5f0b0eb92c
Jake Drzewiecki <jakedrz@Drzewieckis-MacBook-Pro.local>
2025-01-12 20:01:13 -0600

    added main script

---
+++ main.js
@@ -0,0 +1,19 @@
+function main() {
+  console.log("hello!");
+  console.log("the sum of 3 and 2 is");
+
+  //print sum here
+
+  console.log("the product of 3 and 2 is");
+
+  //print product here
+}
+
+function add(x, y) {
+  var sum = 0;
+  return sum;
+}
+
+function multiply(x, y) {
+  //todo return product
+}
